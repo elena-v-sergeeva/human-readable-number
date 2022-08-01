9524dfb874895fa27404e935504b3988f14bcca5
elena-v-sergeeva <75982326+elena-v-sergeeva@users.noreply.github.com>
2022-08-01 15:26:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function toReadable (number) {
-  let result = 0;
-    for(; n; n = Math.floor(n / 10)) {
-        result *= 10;
-        result += n % 10;
-    }
-  
-    return result;
+
 }
